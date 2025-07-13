866db331aec9319dcadf5846aa0ad3efbd17e8b5
levon-dalakyan <wman8800@gmail.com>
2025-07-13 13:29:10 +0300

    add create, delete, sendMessage methods for chat

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"time"
 
-	"github.com/fatih/color"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/levon-dalakyan/chat-server/pkg/chat_v1"
 )
 
+const grpcPort = 50052
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Creating chat for %v", req.GetUsernames())
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Deleting chat, id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Sending message from %s, at %s", req.GetFrom(), req.GetTimestamp().AsTime().Format(time.RFC3339))
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,22 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/levon-dalakyan/chat-server
 
-go 1.23
+go 1.23.0
+
+toolchain go1.24.4
 
 require github.com/fatih/color v1.17.0
 
 require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/grpc v1.73.0 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 )
+
+replace github.com/levon-dalakyan/chat-server/pkg/chat_v1 => ./pkg/chat_v1
