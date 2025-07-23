7dade44071a28c08c68c3b00f29fe0ccc427c906
levon-dalakyan <wman8800@gmail.com>
2025-07-23 11:20:58 +0300

    add tests for service delete

---
+++ internal/service/chats/tests/delete_test.go
@@ -0,0 +1,80 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+
+	"github.com/levon-dalakyan/chat-server/internal/repository"
+	"github.com/levon-dalakyan/chat-server/internal/repository/mocks"
+	"github.com/levon-dalakyan/chat-server/internal/service/chats"
+)
+
+func TestDelete(t *testing.T) {
+	t.Parallel()
+	type chatsRepositoryMockFunc func(mc *minimock.Controller) repository.ChatsRepository
+
+	type args struct {
+		ctx context.Context
+		id  int64
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id = gofakeit.Int64()
+
+		repoErr = fmt.Errorf("repository error")
+	)
+
+	tests := []struct {
+		name                string
+		args                args
+		err                 error
+		chatsRepositoryMock chatsRepositoryMockFunc
+	}{
+		{
+			name: "success case",
+			args: args{
+				ctx: ctx,
+				id:  id,
+			},
+			err: nil,
+			chatsRepositoryMock: func(mc *minimock.Controller) repository.ChatsRepository {
+				mock := mocks.NewChatsRepositoryMock(mc)
+				mock.DeleteMock.Expect(ctx, id).Return(nil)
+				return mock
+			},
+		},
+		{
+			name: "repository error case",
+			args: args{
+				ctx: ctx,
+				id:  id,
+			},
+			err: repoErr,
+			chatsRepositoryMock: func(mc *minimock.Controller) repository.ChatsRepository {
+				mock := mocks.NewChatsRepositoryMock(mc)
+				mock.DeleteMock.Expect(ctx, id).Return(repoErr)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			chatsRepositoryMock := tt.chatsRepositoryMock(mc)
+			service := chats.NewService(chatsRepositoryMock)
+
+			err := service.Delete(tt.args.ctx, tt.args.id)
+			require.Equal(t, tt.err, err)
+		})
+	}
+}
